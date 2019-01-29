539ebf49a37a0aaae927610682e1d1384795bd81
singhjason96 <jason.singh@baruchmail.cuny.edu>
2019-01-28 22:16:00 -0500

    fixed syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_many :stocks    
     has_secure_password
-    def global_stock_update
+    def self.global_stock_update
         puts "Update Stocks Here"
     end
     
-    
+
 
 end
