bdc5704a6f51dd5d7a8056782e5704c82b3605b8
singhjason96 <jason.singh@baruchmail.cuny.edu>
2019-01-30 11:27:11 -0500

    fixed syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
     def stop_alert(live_price, stock)
         p "alerted #{username} of stock stop
         #{stock.ticker} just hit #{live_price} which is under #{stock.stop}"
-        account_sid = ACe89054f9f9aacfdfe4d21134a456f244
+        account_sid = "ACe89054f9f9aacfdfe4d21134a456f244"
         auth_token = ENV["TWILIO_ACCOUNT_AUTH_TOKEN"]
         @message = "Hey  #{stock.ticker} just hit #{live_price} which is under #{stock.stop}"
         @client = Twilio::REST::Client.new(account_sid, auth_token)
